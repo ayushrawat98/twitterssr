2e95b18cf67f3ea0612023c110ffeaceffa7fb10
ayushrawat98 <aayushrawat0@gmail.com>
2025-04-14 19:05:07 +0530

    Angular update

--- public/angular/server/main.server.mjs
+++ public/angular/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QONAZD3J.mjs";import"./chunk-YLT5DCW6.mjs";import"./chunk-QZLY374Y.mjs";import"./chunk-NUHSH56V.mjs";import"./chunk-ND7GK4P5.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-HWL5UM5X.mjs";import"./chunk-GTO22LO2.mjs";import"./chunk-QZLY374Y.mjs";import"./chunk-BEMH4ZAT.mjs";import"./chunk-5XPW4WTW.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
