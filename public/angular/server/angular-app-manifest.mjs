2d4fc1e93f16c5bfcc2ce8ec52321b7c6ae0794c
ayushrawat98 <aayushrawat0@gmail.com>
2025-05-09 19:16:44 +0530

    Angular update

--- public/angular/server/angular-app-manifest.mjs
+++ public/angular/server/angular-app-manifest.mjs
@@ -8,88 +8,88 @@ export default {
   entryPointToBrowserMapping: {
   "src/Components/login/login.component.ts": [
     {
-      "path": "chunk-YZUA553N.js",
+      "path": "chunk-HTUOXGMG.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-4LILKTLM.js",
+      "path": "chunk-GR6YHOP3.js",
       "dynamicImport": false
     }
   ],
   "src/Components/register/register.component.ts": [
     {
-      "path": "chunk-BI7TAGH5.js",
+      "path": "chunk-ARCPNDBR.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-4LILKTLM.js",
+      "path": "chunk-GR6YHOP3.js",
       "dynamicImport": false
     }
   ],
   "src/Components/notification/notification.component.ts": [
     {
-      "path": "chunk-4RKN7UXT.js",
+      "path": "chunk-O6CHSINW.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-RN4H7MPU.js",
+      "path": "chunk-T6DLWQDX.js",
       "dynamicImport": false
     }
   ],
   "src/Components/main/main.component.ts": [
     {
-      "path": "chunk-RJDY6BBC.js",
+      "path": "chunk-2NPKVF5Y.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-OLETZS33.js",
+      "path": "chunk-G25BJCFI.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-4LILKTLM.js",
+      "path": "chunk-GR6YHOP3.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-LEKSXHYF.js",
+      "path": "chunk-EEZQA65K.js",
       "dynamicImport": false
     }
   ],
   "src/Components/profile/profile.component.ts": [
     {
-      "path": "chunk-GVWWJ5BS.js",
+      "path": "chunk-SDEETUKF.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-RN4H7MPU.js",
+      "path": "chunk-T6DLWQDX.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-LEKSXHYF.js",
+      "path": "chunk-EEZQA65K.js",
       "dynamicImport": false
     }
   ],
   "src/Components/postexpanded/postexpanded.component.ts": [
     {
-      "path": "chunk-YRUJEFGP.js",
+      "path": "chunk-X33UZAOX.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-OLETZS33.js",
+      "path": "chunk-G25BJCFI.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-4LILKTLM.js",
+      "path": "chunk-GR6YHOP3.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-LEKSXHYF.js",
+      "path": "chunk-EEZQA65K.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 1387, hash: 'd9cea4aa23a1c1e33bae041296b2370cb5b7af03eb3e069abe2652682860d810', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1612, hash: '839201d609ed2b3f023a2329edfcb8d61b9adf49feb1dd54e89a0c8d3076ca13', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1387, hash: '77698daaca5b258bf8089fdfc8c1120f6c758fcef36b461dff506e2b83a23384', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1612, hash: '4fb31cf41c1ce794ff895f2a34dd25d45f2bc06e184263e6ac5ef533bf683d2f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QBK773LE.css': {size: 983, hash: 'ODbyFzpoQ/o', text: () => import('./assets-chunks/styles-QBK773LE_css.mjs').then(m => m.default)}
   },
 };
