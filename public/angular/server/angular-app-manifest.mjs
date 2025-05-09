5b850781987c29d2d29533cfc750e6fadcf5ec99
ayushrawat98 <aayushrawat0@gmail.com>
2025-05-09 07:51:51 +0530

    Angular update

--- public/angular/server/angular-app-manifest.mjs
+++ public/angular/server/angular-app-manifest.mjs
@@ -8,88 +8,88 @@ export default {
   entryPointToBrowserMapping: {
   "src/Components/login/login.component.ts": [
     {
-      "path": "chunk-BAVLCPAZ.js",
+      "path": "chunk-26RHYXTM.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-7AEAGY6G.js",
+      "path": "chunk-LO7DOOKE.js",
       "dynamicImport": false
     }
   ],
   "src/Components/register/register.component.ts": [
     {
-      "path": "chunk-K2SGRQAS.js",
+      "path": "chunk-YV7GORXI.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-7AEAGY6G.js",
+      "path": "chunk-LO7DOOKE.js",
       "dynamicImport": false
     }
   ],
   "src/Components/notification/notification.component.ts": [
     {
-      "path": "chunk-Q47YG4IE.js",
+      "path": "chunk-VBEETOBT.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-LSRHKETJ.js",
+      "path": "chunk-U6RVHALY.js",
       "dynamicImport": false
     }
   ],
   "src/Components/main/main.component.ts": [
     {
-      "path": "chunk-WLTQKFZP.js",
+      "path": "chunk-KZGOQZNH.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-MGQIIRTV.js",
+      "path": "chunk-IR4LLN35.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-7AEAGY6G.js",
+      "path": "chunk-LO7DOOKE.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-J42UOQLP.js",
+      "path": "chunk-CXRYK3VX.js",
       "dynamicImport": false
     }
   ],
   "src/Components/profile/profile.component.ts": [
     {
-      "path": "chunk-PIUFBSAG.js",
+      "path": "chunk-KFMNZYUO.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-LSRHKETJ.js",
+      "path": "chunk-U6RVHALY.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-J42UOQLP.js",
+      "path": "chunk-CXRYK3VX.js",
       "dynamicImport": false
     }
   ],
   "src/Components/postexpanded/postexpanded.component.ts": [
     {
-      "path": "chunk-SO55ZURU.js",
+      "path": "chunk-G4T4K3GN.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-MGQIIRTV.js",
+      "path": "chunk-IR4LLN35.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-7AEAGY6G.js",
+      "path": "chunk-LO7DOOKE.js",
       "dynamicImport": false
     },
     {
-      "path": "chunk-J42UOQLP.js",
+      "path": "chunk-CXRYK3VX.js",
       "dynamicImport": false
     }
   ]
 },
   assets: {
-    'index.csr.html': {size: 996, hash: '5d84d9af2a4bfd5ce38490e4d32662589716b51addc21240d860c06c7d42499b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1220, hash: '2cbbf6484e854eeb5be3823639ee64de57da21aa9dd431bb130845b43e3c35cb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 996, hash: 'd8c6909a328b65e303cee9ba848089541154792089a60ae9a5dd6e86654fdd26', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1220, hash: '79661f6993709731d95ccbf6e2def6633a58e6e63b79b792ab4330c71b2a3e1a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QBK773LE.css': {size: 983, hash: 'ODbyFzpoQ/o', text: () => import('./assets-chunks/styles-QBK773LE_css.mjs').then(m => m.default)}
   },
 };
--- public/angular/server/assets-chunks/index_csr_html.mjs
+++ public/angular/server/assets-chunks/index_csr_html.mjs
@@ -10,6 +10,6 @@ export default `<!doctype html>
 <style>*{box-sizing:border-box}html,body{height:100%}body{margin:0;padding:0;font-family:Segoe UI,Tahoma,Geneva,Verdana,sans-serif;background-color:#14191f;color:#f5f5f5}</style><link rel="stylesheet" href="styles-QBK773LE.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-QBK773LE.css"></noscript></head>
 <body ngcm="">
   <app-root></app-root>
-<link rel="modulepreload" href="chunk-G5KA7DMQ.js"><link rel="modulepreload" href="chunk-YRDDDRJE.js"><script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-OBLVPK7J.js" type="module"></script></body>
+<link rel="modulepreload" href="chunk-G5KA7DMQ.js"><link rel="modulepreload" href="chunk-3DPVQO5R.js"><script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-AX7PWYDI.js" type="module"></script></body>
 </html>
 `;
