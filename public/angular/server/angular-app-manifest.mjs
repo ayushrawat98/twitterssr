bbf9cd1894a91288f2b4b179856febf531488723
ayushrawat98 <aayushrawat0@gmail.com>
2025-05-12 21:07:03 +0530

    Angular update

--- public/angular/server/angular-app-manifest.mjs
+++ public/angular/server/angular-app-manifest.mjs
@@ -38,7 +38,7 @@ export default {
   ],
   "src/Components/main/main.component.ts": [
     {
-      "path": "chunk-SO6BHD27.js",
+      "path": "chunk-TC3XDR6E.js",
       "dynamicImport": false
     },
     {
@@ -88,8 +88,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 1067, hash: '50a7698ad703888bec6701033b8f87f26707da862acfb5a0564fc2cecce1097a', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1292, hash: 'c42470586f30b257e03f57a070b41fda57b8d9c9768dcd08cc2e25c651aebbc8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 1067, hash: 'c0cb2bbfabb5a9028d820dc1362ee72748f5be2eccba36b533472e8a07ea642e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1292, hash: '01ebf03fec4dede420d0b0a94cd92d94cd6905d68ec1294a53d8518162e4e6a1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-QBK773LE.css': {size: 983, hash: 'ODbyFzpoQ/o', text: () => import('./assets-chunks/styles-QBK773LE_css.mjs').then(m => m.default)}
   },
 };
